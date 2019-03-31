e153ff37beab87d19d37779417e2f3a6a3fc924c
carlos <carlos8390@hotmail.com>
2019-03-31 15:43:11 -0500

    11 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/inicio'
   root 'welcome#inicio'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'inicio' => 'welcome#inicio', as: 'inicio'
+  get 'sobremi' => 'welcome#sobremi', as: 'sobremi'
+  get 'cursos' => 'welcome#cursos', as: 'cursos'
+  get 'servicios' => 'welcome#servicios', as: 'servicios'
+  get 'portafolio' => 'welcome#portafolio', as: 'portafolio'
+  get 'contacto' => 'welcome#contacto', as: 'contacto'
 end
